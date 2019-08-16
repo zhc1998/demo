257a19efa5850fb3fc08a0a95649c0e2114c32c9
xiao199606 <939365782@qq.com>
2019-08-16 15:02:10 +0800

    大修改

--- consumer/src/main/java/com/jk/util/FileUtil.java
+++ consumer/src/main/java/com/jk/util/FileUtil.java
@@ -1,18 +1,15 @@
 package com.jk.util;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 //import org.apache.commons.io.FileUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
+
 //文件工具类
 public class FileUtil {
 	
