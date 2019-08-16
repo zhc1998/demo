3177bbc4f419a371b0b43c8e22bd3ea540f88266
zx55211314 <3184954782@qq.com>
2019-08-16 21:33:11 +0800

    新增路径

--- consumer/src/main/java/com/jk/controller/ToShowController.java
+++ consumer/src/main/java/com/jk/controller/ToShowController.java
@@ -1,7 +1,9 @@
 package com.jk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("toshow")
@@ -21,4 +23,16 @@ public class ToShowController {
         return "commodity";
     }
 
+    //跳转到商品品牌页面
+    @RequestMapping("toDran")
+    public String toDran(){
+        return "dran";
+    }
+
+    //跳转到修改商品页面
+    @RequestMapping("toUpdCommodity")
+    public String toUpdCommodity(Integer id, Model model){
+        model.addAttribute("id",id);
+        return "updCommodity";
+    }
 }
