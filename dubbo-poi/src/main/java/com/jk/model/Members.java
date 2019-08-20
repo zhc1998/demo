91ba3cacd364055895250d43bac55c4f91b224cf
xiao199606 <939365782@qq.com>
2019-08-20 19:30:47 +0800

    修改

---
+++ dubbo-poi/src/main/java/com/jk/model/Members.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (C), 2019-2019, 金科
+ * FileName: Members
+ * Author:  黄斌
+ * Date:     2019/8/16 19:29
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jk.model;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author 斌
+ * @create 2019/8/16
+ * @since 1.0.0
+ */
+public class Members implements Serializable {
+
+    private Integer id;
+    private String username;
+    private String password;
+    private String mimg;
+    private String phone;
+    private Integer gender;
+    private Integer type;
+    private String nickname;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMimg() {
+        return mimg;
+    }
+
+    public void setMimg(String mimg) {
+        this.mimg = mimg;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+}
