bb6b4f08a049f20278d74c7ab1d88e43a633c6de
zhc1998 <1255574787@qq.com>
2019-08-28 14:03:24 +0800

    上传项目

--- consumer/src/main/java/com/jk/controller/ZhcController.java
+++ consumer/src/main/java/com/jk/controller/ZhcController.java
@@ -1,7 +1,16 @@
 package com.jk.controller;
 
 import com.jk.model.Members;
+import com.jk.model.commodity.CommodityModel;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,10 +20,18 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("zhc")
 public class ZhcController {
+
+    @Autowired
+    private SolrClient client;
 
     @RequestMapping("getLoginName")
     @ResponseBody
